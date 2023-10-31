464488caa172a0d9d04af69e03a86fe5a409ba6c
abhishek <alokyadav383@gmail.com>
2023-10-31 17:33:00 +0530

    adding 1st layer

---
+++ airflow-files/scripts/p01_department.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from google.cloud import storage
+from google.cloud import bigquery
+from io import StringIO
+import pandas_gbq
+
+def extract_data_from_gcs(bucket_name, file_name):
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    content = blob.download_as_text()
+    return content
+
+def transform_data(df):
+    department = pd.DataFrame(df["Department"].unique(), columns=["Department"]).reset_index().rename(columns={"index": "DepartmentId"})
+    department_job_role = df["Department"] + "|" + df["JobRole"]
+    department_job_role = department_job_role.unique()
+    dep = []
+    job_role = []
+    for i in range(len(department_job_role)):
+        dep.append(department_job_role[i].split("|")[0])
+        job_role.append(department_job_role[i].split("|")[1])
+    department_job_role = pd.DataFrame({"Department": dep, "JobRole": job_role}).reset_index().rename(columns={"index": "JobId"})
+    department_job_role = pd.merge(department_job_role, department, on="Department", how="left").drop(["Department"], axis=1)
+    return department, department_job_role
+
+def load_data_to_gcs(bucket_name, file_name, data, content_type="text/csv"):
+    client =  storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    blob.upload_from_string(data, content_type=content_type)
+
+def load_data_to_bigquery(dataset_id, table_id, data):
+    client = bigquery.Client()
+    table_ref = f"{dataset_id}.{table_id}"
+    pandas_gbq.to_gbq(data, destination_table=table_ref)
+
+def department_etl():
+    bucket_name = 'person-data-warehouse'
+    file_name = 'general_data.csv'
+
+    # Extraction
+    content = extract_data_from_gcs(bucket_name, file_name)
+
+    # Transformation
+    df = pd.read_csv(StringIO(content))
+    department, department_job_role = transform_data(df)
+
+    # Loading to GCS
+    load_data_to_gcs(bucket_name, "Department.csv", department.to_csv(index=False))
+    load_data_to_gcs(bucket_name, "JobDetails.csv", department_job_role.to_csv(index=False))
+
+    # Loading to BigQuery
+    load_data_to_bigquery("PersonDataWarehouse", "Department", department)
+    load_data_to_bigquery("PersonDataWarehouse", "JobDetails", department_job_role)
+
+if __name__ == "__main__":
+    department_etl()
---
+++ airflow-files/scripts/p01_survey.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from google.cloud import storage
+from google.cloud import bigquery
+from io import StringIO
+import pandas_gbq
+
+def load_data_to_bigquery(dataset_id, table_id, data):
+    client = bigquery.Client()
+    table_ref = f"{dataset_id}.{table_id}"
+    pandas_gbq.to_gbq(data, destination_table=table_ref)
+
+def extract_data_from_gcs(bucket, file_name):
+    blob = bucket.get_blob(file_name)
+    content = blob.download_as_text()
+    return pd.read_csv(StringIO(content))
+
+def transform_survey_data(emp_survey, manager_survey, general_data):
+    emp_survey = emp_survey.fillna(0)
+    manager_survey = manager_survey.fillna(0)
+    rating_mapping = {
+        1: 'Low',
+        2: 'Medium',
+        3: 'High',
+        4: 'Very High'
+    }
+    emp_survey_table = emp_survey[["EnvironmentSatisfaction","JobSatisfaction","WorkLifeBalance"]].drop_duplicates().reset_index(drop=True).reset_index().rename(columns = {"index":"SurveyId"})
+    emp_survey_table = pd.merge(emp_survey_table,emp_survey,on = ["EnvironmentSatisfaction","JobSatisfaction","WorkLifeBalance"])
+    general_data = pd.merge(general_data,emp_survey_table,on = "EmployeeID").drop(columns =["EnvironmentSatisfaction","JobSatisfaction","WorkLifeBalance"])
+    manger_survey_table = manager_survey[['JobInvolvement', 'PerformanceRating']].drop_duplicates().reset_index(drop=True).reset_index().rename(columns = {"index":"SurveyId"})
+    manger_survey_table = pd.merge(manger_survey_table,manager_survey,on = ['JobInvolvement', 'PerformanceRating'])
+    general_data = pd.merge(general_data,manger_survey_table,on = "EmployeeID").drop(columns =['JobInvolvement', 'PerformanceRating']).rename(columns = {"SurveyId_x":"EmployeeSurveyId","SurveyId_y" : "ManagerSurveyId"})
+    emp_survey_columns= ["EnvironmentSatisfaction","JobSatisfaction","WorkLifeBalance"]
+    manager_survey_columns = ["JobInvolvement","PerformanceRating"]
+    emp_survey[emp_survey_columns] = emp_survey[emp_survey_columns].astype(int)
+    manager_survey[manager_survey_columns] = manager_survey[manager_survey_columns].astype(int)
+    emp_survey["EnvironmentSatisfaction"] = emp_survey["EnvironmentSatisfaction"].map(rating_mapping)
+    emp_survey["JobSatisfaction"] = emp_survey["JobSatisfaction"].map(rating_mapping)
+    emp_survey["WorkLifeBalance"] = emp_survey["WorkLifeBalance"].map(rating_mapping)
+    manager_survey["JobInvolvement"] = manager_survey["JobInvolvement"].map(rating_mapping)
+    manager_survey["PerformanceRating"] = manager_survey["PerformanceRating"].map(rating_mapping)
+
+    return emp_survey, manager_survey, general_data
+
+def load_data_to_gcs(bucket, file_name, data, content_type="text/csv"):
+    blob = bucket.blob(file_name)
+    blob.upload_from_string(data, content_type=content_type)
+
+def survey_etl():
+    client = storage.Client()
+    bucket_name = 'person-data-warehouse'
+    bucket = client.get_bucket(bucket_name)
+
+    # Extraction
+    emp_survey = extract_data_from_gcs(bucket, "employee_survey_data.csv")
+    manager_survey = extract_data_from_gcs(bucket, "manager_survey_data.csv")
+    general_data = extract_data_from_gcs(bucket, "general_data.csv")
+
+    # Transformation
+    emp_survey, manager_survey, general_data = transform_survey_data(emp_survey, manager_survey, general_data)
+
+    # Loading to GCS
+    load_data_to_gcs(bucket, "EmployeeSurvey.csv", emp_survey.to_csv(index=False))
+    load_data_to_gcs(bucket, "ManagerSurvey.csv", manager_survey.to_csv(index=False))
+    load_data_to_gcs(bucket, "EmployeeStage.csv", general_data.to_csv(index=False))
+
+    # Loading to BigQuery
+    dataset_id = "PersonDataWarehouse"
+    load_data_to_bigquery(dataset_id, "EmployeeSurvey", emp_survey)
+    load_data_to_bigquery(dataset_id, "ManagerSurvey", manager_survey)
+
+if __name__ == "__main__":
+    survey_etl()
